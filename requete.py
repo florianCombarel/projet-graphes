bdd7202461803b8e3b2ebb66592edb3f3adfa9e0
Unknown <florian.combarel@orange.fr>
2017-12-19 12:10:45 +0100

    modif avec selection des stations à recharger

--- requete.py
+++ requete.py
@@ -1,4 +1,17 @@
 import requests
+import json
 
-r = requests.get("https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=velo-toulouse&lang=fr&geofilter.distance=43.600470%2C+1.445672%2C+750")
+r = requests.get("https://api.jcdecaux.com/vls/v1/stations?contract=Toulouse&apiKey=397a31b163129affc10acc002c5db644c338ad54")
 print(r.content)
+data = r.json()
+#print(json.dumps(data, indent=4))
+#print(data[0]["number"])
+data_filtrer = []
+print("data = "+str(len(data)))
+for x in data:
+    pourcentage = x["available_bikes"] * 100 / x["bike_stands"]
+    if pourcentage < 10:
+        data_filtrer.append(x)
+print("data_filtrer = "+str(len(data_filtrer)))
+print(json.dumps(data_filtrer, indent=4))
+
